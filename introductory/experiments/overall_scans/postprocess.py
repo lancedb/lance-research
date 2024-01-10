8fd2958921614d899ccb620bf04f796a396206d5
Will Jones <willjones127@gmail.com>
2024-01-10 15:22:22 -0800

    Full scan experiment (#14)
    This shows a comparison of Lance against Parquet for scanning TPC-H
    `lineitem` data, showing two of the current weaknesses for OLAP data.
    
    ---------
    
    Co-authored-by: Weston Pace <weston.pace@gmail.com>

---
+++ introductory/experiments/overall_scans/postprocess.py
@@ -0,0 +1,95 @@
+"""Convert benchmark data output by pytest-benchmark into a CSV file."""
+import json
+import csv
+import argparse
+import os
+import typing
+
+
+class Result(typing.NamedTuple):
+    dataset: str
+    format: str
+    row_group_size: int
+    dataset_size_bytes: int
+    scan_time: float
+
+
+def get_size(start_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
+def iter_dataset_sizes():
+    datasets = os.listdir("data")
+    for dataset in datasets:
+        for name in os.listdir(os.path.join("data", dataset)):
+            fmt, group_size = name.split("-")
+            size = get_size(os.path.join("data", dataset, name))
+            yield dict(
+                dataset=dataset,
+                format=fmt,
+                row_group_size=group_size,
+                dataset_size_bytes=size,
+            )
+
+
+def iter_benchmark_data(path):
+    with open(path) as f:
+        results = json.load(f)
+    for benchmark in results["benchmarks"]:
+        # Example: "test_scan_tpch[lance-1K]" -> "lance-1K"
+        bench_parameter = benchmark["name"].split("[")[1][:-1]
+        format, row_group_size = bench_parameter.split("-")
+        yield dict(
+            dataset=benchmark["group"],
+            format=format,
+            row_group_size=row_group_size,
+            benchmark=benchmark,
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type=str)
+    args = parser.parse_args()
+
+    if args.data is None:
+        # Get the latest benchmark data, if not specified
+        benches_directory = os.path.join(".benchmarks", os.listdir(".benchmarks")[0])
+        args.data = os.path.join(benches_directory, sorted(os.listdir(benches_directory))[-1])
+        print("Using latest benchmark data: {}".format(args.data))
+
+    # We need to join to the benchmark data with (dataset, format, row_group_size)
+    dataset_meta = {
+        (d["dataset"], d["format"], d["row_group_size"]): d
+        for d in iter_dataset_sizes()
+    }
+
+    with open("results.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(
+            ["dataset", "format", "row_group_size", "dataset_size_bytes", "scan_time"]
+        )
+        for benchmark in iter_benchmark_data(args.data):
+            meta = dataset_meta[
+                (benchmark["dataset"], benchmark["format"], benchmark["row_group_size"])
+            ]
+
+            group_size = benchmark["row_group_size"]
+            if group_size.endswith("K"):
+                group_size = int(group_size[:-1]) * 1024
+            else:
+                raise NotImplementedError("Only K suffix supported")
+
+            row = Result(
+                dataset=benchmark["dataset"],
+                format=benchmark["format"],
+                row_group_size=group_size,
+                dataset_size_bytes=meta["dataset_size_bytes"],
+                scan_time=benchmark["benchmark"]["stats"]["min"],
+            )
+            writer.writerow(row)
---
+++ introductory/experiments/overall_scans/readme.md
@@ -0,0 +1,5 @@
+# Scan experiments
+
+These experiments demonstrate overall scan performance for Lance versus Parquet on several datasets.
+
+See the [Makefile](./Makefile) for instructions to run the experiments.
